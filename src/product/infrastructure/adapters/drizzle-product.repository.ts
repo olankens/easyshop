79993be73d18f7a6d14dcd459c932ea1f96ce471
olankens <olankens@users.noreply.github.com>
2026-08-26 12:03:51 +0200

    Add ProductRepository port with Drizzle adapter skeleton

---
+++ src/product/infrastructure/adapters/drizzle-product.repository.ts
@@ -0,0 +1,28 @@
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  ProductFilters,
+  ProductRepository,
+} from 'src/product/application/ports/product.repository.port';
+import { Product } from 'src/product/domain/entities/product.entity';
+import { ProductId } from 'src/product/domain/value-objects/product-id.vo';
+import {
+  DRIZZLE,
+  DrizzleDB,
+} from 'src/shared/infrastructure/database/postgres/drizzle.provider';
+
+@Injectable()
+export class DrizzleProductRepostory implements ProductRepository {
+  constructor(@Inject(DRIZZLE) private readonly db: DrizzleDB) {}
+
+  save(product: Product): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  findById(id: ProductId): Promise<Product | null> {
+    throw new Error('Method not implemented.');
+  }
+
+  findAll(filters: ProductFilters): Promise<Product[]> {
+    throw new Error('Method not implemented.');
+  }
+}
